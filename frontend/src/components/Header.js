d80d59d4036f7ed8430977626c63d5f1df20a34b
Yinka Adedire <yinkakun@gmail.com>
2022-09-22 14:35:30 +0100

    format code

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -42,7 +42,7 @@ const LoggedInView = (props) => {
         <li className="nav-item">
           <Link to={`/@${props.currentUser.username}`} className="nav-link">
             <img
-              src={props.currentUser.image || '/placeholder.png'}
+              src={props.currentUser.image || "/placeholder.png"}
               className="user-pic mr-1"
               alt={props.currentUser.username}
             />
