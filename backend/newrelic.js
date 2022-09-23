2d975e1f3da823c772d8ac159a99dc5354052b46
Yinka Adedire <yinkakun@gmail.com>
2022-09-23 22:06:31 +0100

    fix

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -6,15 +6,17 @@ require("dotenv").config();
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
+const appName = process.env.NEW_RELIC_APP_NAME;
+const licenseKey = process.env.NEW_RELIC_LICENSE_KEY;
 exports.config = {
   /**
    * Array of application names.
    */
-  // app_name: [process.env.NEW_RELIC_APP_NAME],
+  app_name: [appName],
   /**
    * Your New Relic license key.
    */
-  // license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  license_key: licenseKey,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
